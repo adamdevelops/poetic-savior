208d3dd1f23259610c6a3b112ce8b7ad5cd42ddd
Adam <adamh93@verizon.net>
2018-11-02 15:09:51 -0400

    done: Imported Poem model in poemsblog views.py file, query Poem model in bloghome route to display in bloghome.html page, Added template tagging to iterate through Poem variable and display all Poem objects on the page

--- rapsite/poemsblog/views.py
+++ rapsite/poemsblog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
+from .models import Poem
 
 
 def bloghome(request):
+    poems = Poem.objects.all().order_by('date')
     return render(request, 'bloghome.html', {'poems': poems})
